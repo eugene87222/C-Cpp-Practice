5900a923a7d0c1b3c54efbd7efc8491485dcd255
Eugene Yang <eugene87222@gmail.com>
2017-07-06 18:03:21 +0800

    modify the code of extended euclidean

--- Extended Euclidean/Extended Euclidean.cpp
+++ Extended Euclidean/Extended Euclidean.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 typedef long long ll;
 
-ll gcd(ll a,ll b,ll&x,ll&y){
+ll ext_euclidean(ll a,ll b,ll&x,ll&y){
     if(!b){
         x=1;
         y=0;
         return a;
     }
-    ll r=gcd(b,a%b,y,x);
+    ll r=ext_euclidean(b,a%b,y,x);
     ll t=x;
     y=y-a/b*t;
     return r;
 }
-int main(){ 
+int main(){
+	ll a=100,b=70;
+	ll x,y,gcd;
+	gcd=ext_euclidean(a,b,x,y);
+	cout<<"a="<<a<<endl<<"b="<<b<<endl;
+	cout<<x<<"*a+"<<y<<"*b="<<gcd<<endl;
     return 0;  
 }  
