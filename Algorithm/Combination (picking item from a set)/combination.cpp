1c1c1ae2ecf5623450e7a45aa4f7ecde4d47ff92
Eugene Yang <eugene87222@gmail.com>
2017-09-04 09:51:26 +0800

    Update combination.cpp

--- Algorithm/Combination (picking item from a set)/combination.cpp
+++ Algorithm/Combination (picking item from a set)/combination.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 /* Pick m elements from a set with n elements (m<=n) */
 #define MAX 20
-void init(int* list,int n){
+void init(int *list,int n){
     for(int i=0;i<n;i++)
     	list[i]=i+1;
 }
-int position(int* list,int m,int n){
+int position(int *list,int m,int n){
     if(list[n-1]!=m){
         return n-1;
     }
@@ -17,16 +17,16 @@ int position(int* list,int m,int n){
         return pos;
     }
 }
-int hasNext(int* list,int m,int n){
+int hasNext(int *list,int m,int n){
     return list[0]<m-n+1;
 }
-void next(int* list,int m,int n){
+void next(int *list,int m,int n){
     int pos=position(list,m,n);
     list[pos]++; 
     for(int i=pos+1;i<n;i++)
     	list[i]=list[i-1]+1;
 }
-void print(int* list,int n){        
+void print(int *list,int n){        
     for(int i=0;i<n;i++)
     	cout<<list[i]<<" ";
     cout<<endl; 
