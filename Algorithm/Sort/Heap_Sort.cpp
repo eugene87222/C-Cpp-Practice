953df122866ebc2a38b8ab05086d6d32ec4a4e7c
Eugene Yang <eugene87222@gmail.com>
2019-08-09 19:37:22 +0800

    Update heap sort

--- Algorithm/Sort/Heap_Sort.cpp
+++ Algorithm/Sort/Heap_Sort.cpp
@@ -1,36 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void max_heapify(int *arr,int start,int end){
-	int dad=start;
-	int son=dad*2+1;
-	while(son<=end){
-		if(son+1<=end&&arr[son]<arr[son+1])
+void max_heapify(int *arr, int start, int end) {
+	int dad = start;
+	int son = dad * 2 + 1;
+	while(son <= end) {
+		if(son + 1 <= end && arr[son] < arr[son + 1])
 			son++;
-		if(arr[dad]>arr[son])
+		if(arr[dad] > arr[son])
 			return;
 		else{
-			swap(arr[dad],arr[son]);
-			dad=son;
-			son=dad*2+1;
+			swap(arr[dad], arr[son]);
+			dad = son;
+			son = dad * 2 + 1;
 		}
 	}
 }
-void heap_sort(int *arr,int len){
-	for(int i=len/2-1;i>=0;i--) /* <Initial> start from last fahter node */
-		max_heapify(arr,i,len-1);
-	for(int i=len-1;i>0;i--){   /* <Sort> */
-		swap(arr[0],arr[i]);
-		max_heapify(arr,0,i-1);
+
+void heap_sort(int *arr, int len) {
+	for(int i = len / 2 - 1; i >= 0; i--) /* <Initial> start from last fahter node */
+		max_heapify(arr, i, len - 1);
+	for(int i = len - 1; i > 0; i--) {   /* <Sort> */
+		swap(arr[0], arr[i]);
+		max_heapify(arr, 0, i - 1);
 	}
 }
-int main(int argc, char const *argv[])
-{
-    int arr[]={3,5,3,0,8,6,1,5,8,6,2,4,9,4,7,0,1,8,9,7,3,1,2,5,9,7,4,0,2,6};
-	int len=(int)sizeof(arr)/sizeof(*arr);
-	heap_sort(arr,len);
-	for(int i=0;i<len;i++)
-		cout<<arr[i]<<" ";
-	cout<<endl;
+
+int main(int argc, char const *argv[]) {
+    int arr[] = {3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6};
+	int len = (int)sizeof(arr) / sizeof(*arr);
+	heap_sort(arr, len);
+	for(int i = 0; i < len; i++)
+		cout << arr[i] << " ";
+	cout << endl;
     return 0; 
 }
