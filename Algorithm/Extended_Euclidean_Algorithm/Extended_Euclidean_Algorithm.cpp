06623545938fa25d762c69462d2aa4bf71daaa20
Eugene Yang <eugene87222@gmail.com>
2019-08-25 19:38:25 +0800

    update ext euclidean

--- Algorithm/Extended_Euclidean_Algorithm/Extended_Euclidean_Algorithm.cpp
+++ Algorithm/Extended_Euclidean_Algorithm/Extended_Euclidean_Algorithm.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int ext_euclidean(int a,int b,int& x,int& y) {
+int ExtEuclidean(int a,int b,int& x,int& y) {
 	/* a * x + b * y = gcd(a, b) */
-	cout << "== CALL ==\n";
     if(b == 0) {
         x = 1;
         y = 0;
         return a;
     }
-    int r = ext_euclidean(b, a % b, y, x);
+    int r = ExtEuclidean(b, a % b, y, x);
     int t = x;
     y = y - a / b * t;
     return r;
@@ -17,8 +17,10 @@ int ext_euclidean(int a,int b,int& x,int& y) {
 
 int main(int argc, char const *argv[]) {
 	int a, b, x, y;
-	cin >> a >> b;
-	int gcd = ext_euclidean(a, b, x, y);
+    ifstream input_file;
+    input_file.open("test.in", fstream::in);
+	input_file >> a >> b;
+	int gcd = ExtEuclidean(a, b, x, y);
 	printf("(%d) * %d + (%d) * %d = %d\n", x, a, y, b, gcd);
 	return 0;
 }
